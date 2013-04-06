ef4fca861fc0f349d5637f19e3a99210687a7bc8
sinmetal <metal.tie@gmail.com>
2013-04-06 21:35:45 +0900

    ApiProxy.Delegateをフックして、Tqがaddされているか確かめてみた。 #2
    Tqにaddはされているが、tester.tasksに入ってないし、Datastoreにも登録はされていない。
    初期状態ではtqにaddするという行為だけが模倣されているのか・・・？

--- src/test/java/org/sinmetal/sample/controller/PutTaskQueueControllerTest.java
+++ src/test/java/org/sinmetal/sample/controller/PutTaskQueueControllerTest.java
@@ -1,6 +1,11 @@
 package org.sinmetal.sample.controller;
 
+import java.util.List;
+import java.util.concurrent.Future;
+
 import org.slim3.tester.ControllerTestCase;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.google.appengine.api.datastore.DatastoreService;
@@ -8,11 +13,87 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.taskqueue.TaskQueuePb.TaskQueueAddRequest;
+import com.google.appengine.api.taskqueue.TaskQueuePb.TaskQueueBulkAddRequest;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.apphosting.api.ApiProxy;
+import com.google.apphosting.api.ApiProxy.ApiConfig;
+import com.google.apphosting.api.ApiProxy.ApiProxyException;
+import com.google.apphosting.api.ApiProxy.Delegate;
+import com.google.apphosting.api.ApiProxy.Environment;
+import com.google.apphosting.api.ApiProxy.LogRecord;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 public class PutTaskQueueControllerTest extends ControllerTestCase {
+
+	LocalServiceTestHelper helper;
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		helper = new LocalServiceTestHelper();
+		helper.setUp();
+		ApiProxy.setDelegate(new TQDelegate());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+		helper.tearDown();
+	}
+
+	static class TQDelegate implements Delegate<Environment> {
+
+		@SuppressWarnings("unchecked")
+		final Delegate<Environment> parent = ApiProxy.getDelegate();
+
+		@Override
+		public void flushLogs(Environment environment) {
+			parent.flushLogs(environment);
+
+		}
+
+		@Override
+		public List<Thread> getRequestThreads(Environment environment) {
+			return parent.getRequestThreads(environment);
+		}
+
+		@Override
+		public void log(Environment environment, LogRecord record) {
+			parent.log(environment, record);
+		}
+
+		@Override
+		public Future<byte[]> makeAsyncCall(Environment environment,
+				String packageName, String methodName, byte[] request,
+				ApiConfig apiConfig) {
+			inspectTaskAddService(packageName, methodName, request);
+			return parent.makeAsyncCall(environment, packageName, methodName,
+					request, apiConfig);
+		}
+
+		@Override
+		public byte[] makeSyncCall(Environment environment, String packageName,
+				String methodName, byte[] request) throws ApiProxyException {
+			inspectTaskAddService(packageName, methodName, request);
+			return parent.makeSyncCall(environment, packageName, methodName,
+					request);
+		}
+
+		void inspectTaskAddService(String packageName, String methodName,
+				byte[] request) {
+			if (packageName.equals("taskqueue") && methodName.equals("BulkAdd")) {
+				TaskQueueBulkAddRequest taskPb = new TaskQueueBulkAddRequest();
+				taskPb.mergeFrom(request);
+				for (int i = 0; i < taskPb.addRequestSize(); i++) {
+					TaskQueueAddRequest addRequest = taskPb.getAddRequest(i);
+					System.out.println("add taskqueue url = " + addRequest.getUrl());
+				}
+			}
+		}
+	}
 
 	@Test
 	public void TaskQueueAddTest() throws Exception {
@@ -21,7 +102,7 @@ public class PutTaskQueueControllerTest extends ControllerTestCase {
 		assertThat(controller, is(notNullValue()));
 		assertThat(tester.isRedirect(), is(false));
 		assertThat(tester.getDestinationPath(), is(nullValue()));
-		
+
 		assertThat(tester.tasks.size(), is(1));
 	}
 
